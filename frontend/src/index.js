5484d581a6865fcb07cc2c7806a181ba8335fd87
Marcos <53981643+Maru-ko@users.noreply.github.com>
2022-10-17 05:31:57 -0400

    AWS AMPLIFY

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Amplify } from 'aws-amplify';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,4 +14,3 @@ root.render(
     </Router>
   </React.StrictMode>
 );
-
